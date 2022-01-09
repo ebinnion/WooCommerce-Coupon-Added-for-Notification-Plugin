001cf1b710d0c56027a5ad182883235368e96ff0
Eric Binnion <ericbinnion@gmail.com>
2022-01-08 23:52:26 -0600

    Use underscores instead of dashes for trigger

--- class-woocommerce-coupon-added.php
+++ class-woocommerce-coupon-added.php
@@ -12,7 +12,7 @@ class WooCommerceCouponAdded extends AbstractTrigger {
 	 */
 	public function __construct() {
 		parent::__construct(
-			'notification-woocommerce-coupon-added',
+			'notification_woocommerce_coupon_added',
 			__( 'Coupon Added', 'notification-woocommerce-coupon-added' )
 		);
 
